1f281fa37d9f5c64b24118c877ec084ed7864124
Inès <ledig.ines@gmail.com>
2026-02-16 16:31:25 +0100

    Home page

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -1,33 +1,39 @@
 <?php
 
-$style = '';
+$style = '<link rel="stylesheet" href="styles/accueil.css">';
 
 ob_start();
 ?>
 
-<a href="index.php?action=escapeGames">Découvrez nos escape games</a>
+<section class="hero-accueil">
+    <div class="hero-accueil__overlay"></div>
+    <div class="hero-accueil__contenu">
+        <h1 class="hero-accueil__titre">VIVEZ L' <span class="hero-accueil__titre--or">AVENTURE</span></h1>
+        <p class="hero-accueil__sous-titre">Plongez dans des univers immersifs, résolvez des énigmes captivantes et échappez-vous avant la fin du temps.</p>
+        <a href="index.php?action=escapeGames" class="hero-accueil__bouton">Découvrir nos escape <span class="hero-accueil__bouton-icone">→</span></a>
+    </div>
+</section>
 
 <?php
 
-if (isset($acces[0]['statut'])){
-    switch($acces[0]['statut']){
-        case "1" :
+if (isset($acces[0]['statut'])) {
+    switch ($acces[0]['statut']) {
+        case "1":
             echo "<div>vous êtes connecté en tant qu'utilisateur</div>
             <a href='index.php?action=deconnexion'>Déconnexion</a>";
-        break;
-        case "2" :
+            break;
+        case "2":
             echo "<div>vous êtes connecté en tant qu'administrateur</div>
             <a href='index.php?action=utilisateurs'>Liste des utilisateurs</a>
             <a href='index.php?action=pageAjoutEscape'>Ajouter un escape game</a>
             <a href='index.php?action=deconnexion'>Déconnexion</a>";
-        break;
-        default :
+            break;
+        default:
             echo "<div>vous n'êtes pas connecté</div>
             <a href='index.php?action=pageConnexion'>Connexion</a>
             <a href='index.php?action=pageInscription'>Inscription</a>";
     }
-}
-else {
+} else {
     echo "<div>vous n'êtes pas connecté</div>
     <a href='index.php?action=pageConnexion'>Connexion</a>
     <a href='index.php?action=pageInscription'>Inscription</a>";
--- vue/vueInscription.php
+++ vue/vueInscription.php
@@ -1,6 +1,6 @@
 <?php
 
-$style = '';
+$style = '<link rel="stylesheet" href="styles/inscription.css">';
 
 ob_start();
 ?>
