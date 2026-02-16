4aa7493db04e37ee49b75bc4fa37164a638b86f2
stantiller <stantiller.1234@gmail.com>
2026-02-16 16:52:35 +0100

    préparation modification infos compte

--- controleur/ctlCompte.class.php
+++ controleur/ctlCompte.class.php
@@ -5,19 +5,23 @@ require_once "vue/vue.class.php";
 
 class ctlCompte {
 
+    private $compte;
+
+    public function __construct(){
+        $this->compte = new compte();
+    }
+
     public function getAcces($acces)
     {
-        $accesUser = new compte();
         $mail = $_SESSION["acces"];
-        $acces = $accesUser->getAcces($mail);
+        $acces = $this->compte->getAcces($mail);
         return $acces;
     }
 
     public function getMail($mail)
     {
-        $mailUser = new compte();
         $mail = $_SESSION["acces"];
-        $mailUtilisateur = $mailUser->getMail($mail);
+        $mailUtilisateur = $this->compte->getMail($mail);
         return $mailUtilisateur;
     }
 
@@ -26,5 +30,38 @@ class ctlCompte {
         session_destroy(); // Détruit la session
         header("Location: index.php");
     }
+
+
+    // Modification des informations utilisateur
+
+    // public function afficherUtilisateurs($message, $mail){
+    //     $infos = $this->compte->infosUtilisateur($mail);
+
+    //     $vue = new vue("InfosCompte"); // Instancie la vue appropriée
+    //     $vue->afficher(array("infos" => $infos, "message" => $message));
+    // }
+
+    // function modifInfos($mail, $mdp)
+    // {
+    //     $this->compte->modifInfos($mail);
+
+    //     $infos = $this->compte->infosUtilisateur($mail);
+
+    //     $mdpUtilisateur = $this->compte->getMdp($mail);
+    
+    //     // if(password_verify($mdp, $mdpUtilisateur[0]['mdp'])){
+    //         // $_SESSION["acces"] = $mail;
+    //     // 
+    //         // if(isset($_COOKIE["page"]))
+    //             // header("Location: index.php".$_COOKIE["page"]);
+    //         // else
+    //             // header("location: index.php");
+    //     // }
+    //     // else
+    //         // $vue->afficher(array("message" => "<span>Mot de passe incorrect</span>"));
+    
+    //     $vue = new vue("InfosCompte"); // Instancie la vue appropriée
+    //     $vue->afficher(array("infos" => $infos, "message" => "<span>Le ou les changement(s) ont été réalisé(s) avec succès</span>"));
+    // }
 
 }
