58c9d9e5af54fff980094a5642cef7b1c2b4b122
Inès <ledig.ines@gmail.com>
2026-02-16 13:56:36 +0100

    Modifications

--- composants/header/header_deconnecte.php
+++ composants/header/header_deconnecte.php
@@ -17,7 +17,7 @@
                 <line x1="16" y1="2" x2="16" y2="6"></line>
                 <line x1="8" y1="2" x2="8" y2="6"></line>
                 <line x1="3" y1="10" x2="21" y2="10"></line>
-            </svg> Réserver
+            </svg>Réserver
         </a>
     </div>
 </header>
