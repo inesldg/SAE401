05125f20bc10ab586764f1949c331a5ba12cb49e
LauryneMon <lauryne.mon@gmail.com>
2026-02-18 11:38:06 +0100

    page a propos

---
+++ controleur/ctlPropos.class.php
@@ -0,0 +1,46 @@
+<?php
+require_once "vue/vue.class.php";
+
+
+class ctlPropos
+{
+
+    // Cette méthode manquait et causait l'erreur
+    public function pagePropos()
+    {
+        $styleCustom = '<link rel="stylesheet" href="styles/about.css">';
+        $vue = new vue("Propos");
+        $vue->afficher(array("style" => $styleCustom)); // On passe le style ici
+    }
+
+    public function accueil($acces)
+    {
+        $vue = new vue("Accueil"); // Instancie la vue appropriée
+        $vue->afficher(array("acces" => $acces));
+    }
+
+    public function erreur($message)
+    {
+        $vue = new vue("Erreur");
+        $vue->afficher(array("message" => $message));
+    }
+
+    public function pageInscription($message)
+    {
+        $vue = new vue("Inscription");
+        $vue->afficher(array("message" => $message));
+    }
+
+    public function pageConnexion($message)
+    {
+        $vue = new vue("Connexion");
+        $vue->afficher(array("message" => $message));
+    }
+
+    public function pageAjoutEscape($message)
+    {
+        $vue = new vue("AjoutEscape");
+        $vue->afficher(array("message" => $message));
+    }
+
+}
--- controleur/routeur.class.php
+++ controleur/routeur.class.php
@@ -7,8 +7,10 @@ require "controleur/ctlInscription.class.php";
 require "controleur/ctlConnexion.class.php";
 require "controleur/ctlUtilisateurs.class.php";
 require "controleur/ctlAjoutEscape.class.php";
+require "controleur/ctlPropos.class.php";
 
-class routeur {
+class routeur
+{
     private $ctlPages;
     private $ctlEscapeGames;
     private $ctlCompte;
@@ -17,8 +19,11 @@ class routeur {
     private $ctlUtilisateurs;
     private $ctlAjoutEscape;
 
+    private $ctlPropos;
 
-    public function __construct(){
+
+    public function __construct()
+    {
         $this->ctlPages = new ctlPages();
         $this->ctlEscapeGames = new ctlEscapeGames();
         $this->ctlCompte = new ctlCompte();
@@ -26,122 +31,129 @@ class routeur {
         $this->ctlConnexion = new ctlConnexion();
         $this->ctlUtilisateurs = new ctlUtilisateurs();
         $this->ctlAjoutEscape = new ctlAjoutEscape();
+        $this->ctlPropos = new ctlPropos();
+
     }
 
-    public function routerRequete(){
+    public function routerRequete()
+    {
         try {
 
             if (isset($_SESSION["acces"])) {
 
                 $mail = $_SESSION["acces"];
                 $acces = $this->ctlCompte->getAcces($mail);
-                    
-                    if (isset($_GET["action"])) {
-                        switch($_GET["action"]){
-                            case "deconnexion" :
-                                $this->ctlCompte->deconnexion();
-                            break;
-                            case "accueil" :
-                                $this->ctlPages->accueil($acces);
-                            break;
-                            case "escapeGames" :
-                                $this->ctlEscapeGames->pageEscapeGames();
-                            break;
-                            case "game" :
-                                $this->ctlEscapeGames->pageGame($_GET['idEscapeGame']);
-                            break;
-
-
-                            /********** Pages administrateur **********/
-                            case "utilisateurs" :
-                                if ($acces[0]['statut'] !== 2)
-                                    throw new Exception("Action non valide");
-                                else
-                                    $this->ctlUtilisateurs->afficherUtilisateurs($message="", $mail);
-                            break;
-                            case "pageAjoutEscape" :
-                                if ($acces[0]['statut'] !== 2)
-                                    throw new Exception("Action non valide");
-                                else
-                                    $this->ctlPages->pageAjoutEscape($message="");
-                            break;
-                            case "ajoutEscape" :
-                                if ($acces[0]['statut'] !== 2)
-                                    throw new Exception("Action non valide");
-                                else {
-                                    if (isset($_POST['nom'], $_POST['description'], $_POST['lieu'], $_POST['duree'], $_POST['pers_min'], $_POST['pers_max']))
-                                        $this->ctlAjoutEscape->ajoutEscape($_POST['nom'], $_POST['description'], $_POST['lieu'], $_POST['duree'], $_POST['pers_min'], $_POST['pers_max']);
-                                    else
-                                        $this->ctlPages->pageAjoutEscape($message="<span>Veuillez remplir tout les champs</span>");
-                                }
-                            break;
-                            case "changementAcces" :
-                                if ($acces[0]['statut'] !== 2)
-                                    throw new Exception("Action non valide");
-                                else {
-                                    if (isset($_POST['niveauAcces']) && $_POST['niveauAcces'] !== 0)
-                                        if (isset ($_GET['id']))
-                                            $this->ctlUtilisateurs->changementAcces($_POST['niveauAcces'], $_GET['id'], $mail);
-                                        else
-                                            $this->ctlUtilisateurs->afficherUtilisateurs($message="<span>Veuillez choisir un niveau d'acces si vous souhaitez le modifier sur cet utilisateur</span>", $mail);
-                                    else
-                                        $this->ctlUtilisateurs->afficherUtilisateurs($message="<span>Veuillez choisir un niveau d'acces si vous souhaitez modifier cet utilisateur</span>", $mail);
-                                }
-                            break;
-                            default :
-                                throw new Exception("Action non valide");
-                        }
-                    }
-                    else
-                        $this->ctlPages->accueil($acces);
-            }
-        
-            else {
 
                 if (isset($_GET["action"])) {
-        
-                    switch($_GET["action"]){
-                        case "pageInscription" :
-                            $this->ctlPages->pageInscription($message="");
-                        break;
-                        case "pageConnexion" :
-                            $this->ctlPages->pageConnexion($message="");
-                        break;
-                        case "inscription" :
-                            if (isset($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['mdp'], $_POST['mdpConfirm']))
-                                {
-                                    if (isset($_POST['phone']))
-                                        $phone = $_POST['phone'];
-                                    else
-                                        $phone = 'NULL';
-                                    $this->ctlInscription->inscription($_POST['nom'], $_POST['prenom'], $_POST['mail'], $phone, $_POST['mdp'], $_POST['mdpConfirm']);
-                                }
+                    switch ($_GET["action"]) {
+                        case "deconnexion":
+                            $this->ctlCompte->deconnexion();
+                            break;
+                        case "accueil":
+                            $this->ctlPages->accueil($acces);
+                            break;
+                        case "escapeGames":
+                            $this->ctlEscapeGames->pageEscapeGames();
+                            break;
+                        case "game":
+                            $this->ctlEscapeGames->pageGame($_GET['idEscapeGame']);
+                            break;
+
+                        case "propos":
+                            $this->ctlPropos->pagePropos();
+                            break;
+
+
+                        /********** Pages administrateur **********/
+                        case "utilisateurs":
+                            if ($acces[0]['statut'] !== 2)
+                                throw new Exception("Action non valide");
+                            else
+                                $this->ctlUtilisateurs->afficherUtilisateurs($message = "", $mail);
+                            break;
+                        case "pageAjoutEscape":
+                            if ($acces[0]['statut'] !== 2)
+                                throw new Exception("Action non valide");
+                            else
+                                $this->ctlPages->pageAjoutEscape($message = "");
+                            break;
+                        case "ajoutEscape":
+                            if ($acces[0]['statut'] !== 2)
+                                throw new Exception("Action non valide");
+                            else {
+                                if (isset($_POST['nom'], $_POST['description'], $_POST['lieu'], $_POST['duree'], $_POST['pers_min'], $_POST['pers_max']))
+                                    $this->ctlAjoutEscape->ajoutEscape($_POST['nom'], $_POST['description'], $_POST['lieu'], $_POST['duree'], $_POST['pers_min'], $_POST['pers_max']);
                                 else
-                                    $this->ctlPages->pageInscription($message="<span>Veuillez remplir tout les champs</span>");
-                        break;
-                        case "connexion" :
+                                    $this->ctlPages->pageAjoutEscape($message = "<span>Veuillez remplir tout les champs</span>");
+                            }
+                            break;
+                        case "changementAcces":
+                            if ($acces[0]['statut'] !== 2)
+                                throw new Exception("Action non valide");
+                            else {
+                                if (isset($_POST['niveauAcces']) && $_POST['niveauAcces'] !== 0)
+                                    if (isset($_GET['id']))
+                                        $this->ctlUtilisateurs->changementAcces($_POST['niveauAcces'], $_GET['id'], $mail);
+                                    else
+                                        $this->ctlUtilisateurs->afficherUtilisateurs($message = "<span>Veuillez choisir un niveau d'acces si vous souhaitez le modifier sur cet utilisateur</span>", $mail);
+                                else
+                                    $this->ctlUtilisateurs->afficherUtilisateurs($message = "<span>Veuillez choisir un niveau d'acces si vous souhaitez modifier cet utilisateur</span>", $mail);
+                            }
+                            break;
+                        default:
+                            throw new Exception("Action non valide");
+                    }
+                } else
+                    $this->ctlPages->accueil($acces);
+            } else {
+
+                if (isset($_GET["action"])) {
+
+                    switch ($_GET["action"]) {
+                        case "pageInscription":
+                            $this->ctlPages->pageInscription($message = "");
+                            break;
+
+                        // pour l'accès public :
+                        case "propos":
+                            $this->ctlPropos->pagePropos();
+                            break;
+
+                        case "pageConnexion":
+                            $this->ctlPages->pageConnexion($message = "");
+                            break;
+                        case "inscription":
+                            if (isset($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['mdp'], $_POST['mdpConfirm'])) {
+                                if (isset($_POST['phone']))
+                                    $phone = $_POST['phone'];
+                                else
+                                    $phone = 'NULL';
+                                $this->ctlInscription->inscription($_POST['nom'], $_POST['prenom'], $_POST['mail'], $phone, $_POST['mdp'], $_POST['mdpConfirm']);
+                            } else
+                                $this->ctlPages->pageInscription($message = "<span>Veuillez remplir tout les champs</span>");
+                            break;
+                        case "connexion":
                             if (isset($_POST['mail'], $_POST['mdp']))
                                 $this->ctlConnexion->connexion($_POST['mail'], $_POST['mdp']);
                             else
-                                $this->ctlPages->pageConnexion($message="<span>Veuillez remplir tout les champs</span>");
-                        break;
-                        case "accueil" :
-                            $this->ctlPages->accueil($acces="0");
-                        break;
-                        case "escapeGames" :
+                                $this->ctlPages->pageConnexion($message = "<span>Veuillez remplir tout les champs</span>");
+                            break;
+                        case "accueil":
+                            $this->ctlPages->accueil($acces = "0");
+                            break;
+                        case "escapeGames":
                             $this->ctlEscapeGames->pageEscapeGames();
-                        break;
-                        case "game" :
+                            break;
+                        case "game":
                             $this->ctlEscapeGames->pageGame($_GET['idEscapeGame']);
-                        break;
+                            break;
 
-                        default :
+                        default:
                             throw new Exception("Action non valide");
                     }
-                }
-                else
-                    $this->ctlPages->accueil($acces="0");
-                
+                } else
+                    $this->ctlPages->accueil($acces = "0");
+
             }
 
         } catch (Exception $e) {
