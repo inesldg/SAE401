9b448154cb662cfd7cf52fdf5f0a4f14299000bb
Inès <ledig.ines@gmail.com>
2026-03-02 15:00:33 +0100

    Ajout trad manquantes

--- vue/vueGame.php
+++ vue/vueGame.php
@@ -60,14 +60,19 @@ $style = '<link rel="stylesheet" href="styles/infoEscape.css">';
                         <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
                         <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
                     </svg>
-                    De <?= $escapeGame[0]['nbr_pers_min'] ?> à <?= $escapeGame[0]['nbr_pers_max'] ?> joueurs
+                    <span id="labelParticipants1">De </span>
+                    <?= $escapeGame[0]['nbr_pers_min'] ?>
+                    <span id="labelParticipants2">à </span>
+                    <?= $escapeGame[0]['nbr_pers_max'] ?>
+                    <span id="labelParticipants3">joueurs </span>
                 </div>
                 <div><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
                         stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <circle cx="12" cy="12" r="10"></circle>
                         <polyline points="12 6 12 12 16 14"></polyline>
                     </svg>
-                    Durée de <?= $escapeGame[0]['duree'] ?>h
+                    <span id="labelDuree">Durée de</span>
+                    <?= $escapeGame[0]['duree'] ?>h
                 </div>
                 <div id="lieuGames"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
                         fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
