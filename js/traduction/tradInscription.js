b00a944f6180076c302a5ae1e8d5aa0de01e0850
LauryneMon <lauryne.mon@gmail.com>
2026-03-11 10:25:32 +0100

    js okkkkkkokkkkk

--- js/traduction/tradInscription.js
+++ js/traduction/tradInscription.js
@@ -8,6 +8,17 @@
         "#sinscrireInscr2": { "fr": "S'inscrire", "en": "Sign up", "de": "Registrieren" },
         "#mdpInscrip": { "fr": "Mot de passe", "en": "Password", "de": "Passwort" },
         "#confmpdInscrip": { "fr": "Confirmez le mot de passe :", "en": "Confirm Password :", "de": "Passwort bestätigen :" },
-        "#seconnecInscrip": { "fr": "Déjà un compte ? Se connecter", "en": "Already an account ? Log in", "de": "Bereits ein Konto? Einloggen" }
+        "#seconnecInscrip": { "fr": "Déjà un compte ? Se connecter", "en": "Already an account ? Log in", "de": "Bereits ein Konto? Einloggen" },
+        // ******************************** required *************************************************
+        "#inputmdp_error": { "fr": "Veuillez entrer votre mot de passe.", "en": "Please enter your password." },
+        "#inputdescription_error": { "fr": "Veuillez compléter ce champs.", "en": "Please complete this field." },
+        "#inputlieu_error": { "fr": "Veuillez renseigner un lieu", "en": "Please enter a location." },
+        "#inputduree_error": { "fr": "Veuillez indiquer la durée de l'escape.", "en": "Please indicate the duration of the escape." },
+        "#inputpersmin_error": { "fr": "Veuillez indiquer le nombre de personnes minimum.", "en": "Please indicate the minimum number of people." },
+        "#inputpersmax_error": { "fr": "Veuillez indiquer le nombre de personnes maximum.", "en": "Please indicate the maximum number of people." },
+        "#inputemail_error": { "fr": "Veuillez indiquer une adresse mail valide.", "en": "Please indicate a valid email address." },
+        "#inputconfmdp_error": { "fr": "Veuillez confirmer votre mot de passe.", "en": "Please confirm your password." },
+        "#inputprenom_error": { "fr": "Veuillez renseigner votre prénom.", "en": "Please enter your first name." },
+        "#inputnom_error": { "fr": "Veuillez renseigner votre nom.", "en": "Please fill in your name." }
     });
 })();
