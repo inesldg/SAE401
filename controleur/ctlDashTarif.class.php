04d1ca812141b3aaa0058afab91991daec3e91c2
Emilie <emilie.deiber@uha.fr>
2026-03-11 10:47:22 +0100

    ctlTarif dash style

--- controleur/ctlDashTarif.class.php
+++ controleur/ctlDashTarif.class.php
@@ -4,29 +4,34 @@ require_once "vue/vue.class.php";
 
 class ctlDashTarif
 {
+    // Instance de la classe DashTarif
     private $dashTarif;
 
+    // Constructeur
+    // Crée une instance de DashTarif pour pouvoir utiliser ses méthodes
     public function __construct()
     {
         $this->dashTarif = new DashTarif();
     }
 
-    // Affiche le dashboard des tarifs
+    // Affiche le dashboard des tarifs pour tous les escape games
     public function afficherDashboard()
     {
-        $escapes = $this->dashTarif->getEscapes(); // ← corrigé
+        $escapes = $this->dashTarif->getEscapes();  // Instancie la vue spécifique aux tarifs
 
-        $vue = new Vue("DashboardTarif");
-        $vue->afficher(['escapes' => $escapes, 'dashTarif' => $this->dashTarif]);
+        $vue = new Vue("DashboardTarif");   // Passe la liste des escapes à la vue
+        $vue->afficher(['escapes' => $escapes, 'dashTarif' => $this->dashTarif]);   // Passe l'instance pour utiliser les méthodes dans la vue
     }
 
     // Enregistre ou met à jour les tarifs soumis via le formulaire
     public function enregistrerTarifs()
     {
+        // Vérifie que le formulaire a été soumis et contient les données attendues
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_escape'], $_POST['prix']) && is_array($_POST['prix'])) {
             $id_escape = intval($_POST['id_escape']);
             $prixData = $_POST['prix'];
 
+            // Parcourt chaque tarif pour le sauvegarder
             foreach ($prixData as $effectif => $prix) {
                 $effectif = intval($effectif);
                 $prix = floatval($prix); // si décimal possible
@@ -37,6 +42,7 @@ class ctlDashTarif
             header("Location: index.php?action=dashTarif&success=1");
             exit;
         } else {
+            // Redirection avec message d'erreur si les données ne sont pas correctes
             header("Location: index.php?action=dashTarif&error=1");
             exit;
         }
