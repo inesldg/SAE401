9115e367cfed58c818e393dd723927d31371f9ff
Emilie <emilie.deiber@uha.fr>
2026-03-11 10:41:49 +0100

    ctl admin style

--- controleur/ctlAdmin.class.php
+++ controleur/ctlAdmin.class.php
@@ -1,48 +1,52 @@
 <?php
 
 // Controleur pour afficher le tableau de bord pour l'admin
-
 require_once "modele/admin.class.php";
 require_once "vue/vue.class.php";
 
 class ctlAdmin
 {
-
+    // Instance de la classe admin pour interagir avec la base de données
     private $admin;
 
+    // Constructeur
+    // Crée une instance de la classe admin
     public function __construct()
     {
         $this->admin = new admin();
     }
 
+    // Méthode pour afficher le tableau de bord
     public function afficherDash()
-{
-    $reservations = $this->admin->getReservations();
-    $utilisateurs = $this->admin->getUtilisateurs();
-    $escapes = $this->admin->getEscapes();
-    $noteMoyenne = $this->admin->getNoteMoyenne();
-    $occupation = $this->admin->getTauxOccupation();
+    {
+        $reservations = $this->admin->getReservations();
+        $utilisateurs = $this->admin->getUtilisateurs();
+        $escapes = $this->admin->getEscapes();
+        $noteMoyenne = $this->admin->getNoteMoyenne();
+        $occupation = $this->admin->getTauxOccupation();
 
-    $mois = date("m");
-    $annee = date("Y");
+        $mois = date("m");
+        $annee = date("Y");
 
-    $revenus = $this->admin->getRevenus($mois, $annee);
+        $revenus = $this->admin->getRevenus($mois, $annee);
 
-    // AJOUT
-    $reservationsRecente = $this->admin->getReservationsRecentes();
+        // AJOUT : récupère les réservations récentes
+        $reservationsRecente = $this->admin->getReservationsRecentes();
 
-    $vue = new vue("Dashboard");
+        // Création d'une instance de la vue "Dashboard"
+        $vue = new vue("Dashboard");
 
-    $vue->afficher(array(
-        "reservations" => $reservations['total'],
-        "utilisateurs" => $utilisateurs['total'],
-        "escapes" => $escapes['total'],
-        "revenus" => $revenus['revenus'],
-        "noteMoyenne" => $noteMoyenne['moyenne'],
-        "occupation" => $occupation['totalPers'],
+        // Affichage de la vue avec les données passées dans un tableau associatif
+        $vue->afficher(array(
+            "reservations" => $reservations['total'],
+            "utilisateurs" => $utilisateurs['total'],
+            "escapes" => $escapes['total'],
+            "revenus" => $revenus['revenus'],
+            "noteMoyenne" => $noteMoyenne['moyenne'],
+            "occupation" => $occupation['totalPers'],
 
-        // AJOUT
-        "reservationsRecente" => $reservationsRecente
-    ));
-}
+            // AJOUT : réservations récentes pour le tableau ou les notifications
+            "reservationsRecente" => $reservationsRecente
+        ));
+    }
 }
