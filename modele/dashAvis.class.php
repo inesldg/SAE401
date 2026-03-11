fc53c8a1f0b36b76ba1ec9246073fece631d0f5d
Emilie <emilie.deiber@uha.fr>
2026-03-11 10:40:03 +0100

    com dash avis modele

--- modele/dashAvis.class.php
+++ modele/dashAvis.class.php
@@ -6,6 +6,11 @@ class dashAvis extends database
     // Récupère tous les avis avec les infos utilisateur et escape
     public function getAvis()
     {
+        // Requête SQL pour sélectionner :
+        // - l'ID de l'avis, la note, le commentaire, la date
+        // - le nom et prénom de l'utilisateur
+        // - le nom de l'escape game
+        // Les avis sont triés par ID décroissant (du plus récent au plus ancien)
         $req = "
             SELECT 
                 a.id_avis,
@@ -24,9 +29,11 @@ class dashAvis extends database
         return $this->execReq($req);
     }
 
-    // Supprime un avis par son ID
+    // Supprime un avis selon son ID
     public function supprimerAvis($id)
     {
+        // Requête préparée pour supprimer l'avis correspondant à l'ID fourni
+        // L'utilisation d'une requête préparée sécurise contre les injections SQL
         $req = "DELETE FROM evaluer WHERE id_avis = ?";
         return $this->execReqPrep($req, [$id]);
     }
